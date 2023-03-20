d0c7c645356eefb7f79efe8ff1560171c76af4e9
fsangregorio <103077244+fsangregorio@users.noreply.github.com>
2023-03-19 21:16:17 -0300

    Update primeraEntrega.js

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -1,59 +1,54 @@
 
 class ProductManager
-{
-    getProducts = []
-    product = [];
+{  
+    products = [];
     idAuto = 0;
 
-    getProducts(id)
+    getProducts()
     {
       return this.products;
     }
 
     addProduct(product)
     {
-
        this.products.push({
          ...product,
-         
-         title: new product(),
-         description: [],
-         price: [],
-         thumbnail: [],
-         stock: [],
-         id: this.idAuto ,
+         id: this.idAuto
        });
 
        this.idAuto = this.idAuto + 1;
     }
+  
+    getProductsById(id){  
+        const search = this.products.find((product) => product.id === id);
+        return this.product;
+};
+
 }
 
 const productManager = new ProductManager();
 
-ProductManager.addProduct({ title: 'Pelota',
+productManager.addProduct({ title: 'Pelota',
                             description: 'Pelota de fútbol',
                             price: 5000,
                             thumbnail: 'img1.jpg',
                             stock: 200,
-                            id: this.idAuto
-});
+                            id: this.idAuto });
 
-ProductManager.addProduct({ title: 'raqueta',
+productManager.addProduct({ title: 'Raqueta',
                               description: 'Raqueta de Tenis',
                               price: 10000,
                               thumbnail: 'img2.jpg',
                               stock: 100,
-                              id: this.idAuto
-});
+                              id: this.idAuto });
 
-ProductManager.addProduct({ title: 'botines',
+productManager.addProduct({ title: 'Botines',
                                 description: 'Botines de futbol',
                                 price: 15000,
                                 thumbnail: 'img3.jpg',
                                 stock: 10,
-                                id: this.idAuto
-});
+                                id: this.idAuto });
 
-console.log(ProductManager.getProducts(1));
-console.log(ProductManager.getProducts(2));
-console.log(ProductManager.getProducts(3));
+
+console.log(productManager.getProducts());
+console.log(productManager.getProductsById(1));
